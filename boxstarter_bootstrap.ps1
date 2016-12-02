846052684ada465cb6fcabdb0d7e59b560a16495
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-02 15:54:38 -0800

    yada-yada

--- boxstarter_bootstrap.ps1
+++ boxstarter_bootstrap.ps1
@@ -1,10 +1,11 @@
+mkdir -force c:\windows\setup | out-null
+cd \windows\setup
+
 schtasks /delete /f /tn WindowsUpdateLoop
 schtasks /create /sc onlogon /tn boxstarter /tr "cmd /c start /d %windir%\setup cmd /c C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe -file update.ps1"
 
 if(-not(test-path $env:appdata\Roaming\Boxstarter))
 {
-	mkdir -force c:\windows\setup | out-null
-	cd \windows\setup
 	powershell -noninteractive -noprofile -executionpolicy unrestricted -command "(new-object System.Net.WebClient).DownloadFile('http://boxstarter.org/downloads/Boxstarter.2.8.29.zip','Boxstarter.zip')"
 	powershell -NoProfile -ExecutionPolicy unrestricted -Command "(new-object System.Net.WebClient).DownloadFile('http://installer-bin.streambox.com/7za.exe','7za.exe')"
 	wget -nv -N --no-check-certificate https://raw.githubusercontent.com/TaylorMonacelli/boxstarter-kickstart/tm/wip/update.ps1 -O c:\windows\setup\update.ps1
