66178fc2a1898a42bb35d49071258b107da2b715
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-02 15:32:49 -0800

    t

--- update.ps1
+++ update.ps1
@@ -1,17 +1,19 @@
 if(!(test-path "$env:appdata\Boxstarter\BoxstarterShell.ps1"))
 {
-
 	mkdir -force c:\windows\setup | out-null
 	cd \windows\setup
 	powershell -noninteractive -noprofile -executionpolicy unrestricted -command "(new-object System.Net.WebClient).DownloadFile('http://boxstarter.org/downloads/Boxstarter.2.8.29.zip','Boxstarter.zip')"
 	powershell -NoProfile -ExecutionPolicy unrestricted -Command "(new-object System.Net.WebClient).DownloadFile('http://installer-bin.streambox.com/7za.exe','7za.exe')"
+
+# FIXME: Github doesn't support timestamps
+#	wget -nv -N --no-check-certificate https://raw.githubusercontent.com/TaylorMonacelli/boxstarter-kickstart/tm/wip/update.ps1 -O c:\windows\setup\update.ps1
 	wget -nv -N --no-check-certificate https://raw.githubusercontent.com/TaylorMonacelli/boxstarter-kickstart/tm/wip/update.ps1 -O c:\windows\setup\update.ps1
 	.\7za.exe x -y -obs Boxstarter.zip
 	cd bs
 	cmd /c setup.bat -force
 
-
 }
+
 
 
 . "$env:appdata\Boxstarter\BoxstarterShell.ps1"
