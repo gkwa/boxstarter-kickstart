a0f8eb8abd0cea5bc5601a84f28b1c5ed810703b
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-02 13:02:01 -0800

    yada-yada

--- update_machine.ps1
+++ update_machine.ps1
@@ -1,14 +1,23 @@
 if(-not(test-path $env:appdata\Roaming\Boxstarter)){
 
-	mkdir c:\windows\setup
+	mkdir -force c:\windows\setup
 	cd \windows\setup
-
-	powershell -noninteractive -noprofile -executionpolicy unrestricted -command "(new-object System.Net.WebClient).DownloadFile('http://installer-bin.streambox.com/wget.exe','wget.exe')"
 	powershell -noninteractive -noprofile -executionpolicy unrestricted -command "(new-object System.Net.WebClient).DownloadFile('http://boxstarter.org/downloads/Boxstarter.2.8.29.zip','Boxstarter.zip')"
 	powershell -NoProfile -ExecutionPolicy unrestricted -Command "(new-object System.Net.WebClient).DownloadFile('http://installer-bin.streambox.com/7za.exe','7za.exe')"
 	.\7za.exe x -y -obs Boxstarter.zip
 	cd bs
 	cmd /c setup.bat -force
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
